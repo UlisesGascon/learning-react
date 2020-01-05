5d1a885cb3a273d80f6421c52a83ebb3df36c170
Ulises Gascon <ulises.gascon@guidesmiths.com>
2020-01-05 19:16:08 +0100

    Added Ajax Call
    - Changed Style for link
    - Added hooks for state
    - Added hooks for Ajax Request

--- examples/simple-api/src/App.js
+++ examples/simple-api/src/App.js
@@ -1,31 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import WebFont from 'webfontloader';
 import chuckPic from './chuck_norris.png';
 import './App.css';
-
 
 WebFont.load({
   google: {
     families: ['Acme:400', 'sans-serif']
   }
 });
-
 function App() {
+  const [quote, setQuote] = useState()
+
+  useEffect(() => {
+    fetch("https://api.chucknorris.io/jokes/random")
+      .then(res => res.json())
+      .then(data => {
+        console.log("Data is here", data)
+        setQuote(data.value)
+      })
+      .catch(err => {
+        console.warn(err)
+      })
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={chuckPic} className="App-pic" alt="logo" />
-        <p>
-        Chuck Norris can make you cry with his unbelievable armpit rendition of 'The Star Spangled Banner'.
-        </p>
-        <a
-          className="App-link"
-          href="https://chucknorris.io"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Quote by chucknorris.io
-        </a>
+        {!quote ? (
+        <h1>Loading....</h1>
+        ) : (
+        <React.Fragment>
+          <img src={chuckPic} className="App-pic" alt="logo" />
+          <p>
+          {quote}
+          </p>
+          <a
+            className="App-link"
+            href="https://chucknorris.io"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Quote by chucknorris.io
+          </a>
+        </React.Fragment>
+        )}
+
       </header>
     </div>
   );

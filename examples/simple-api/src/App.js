d0af1434008c8cc16201d462918c05957caf0daa
Ulises Gascon <ulises.gascon@guidesmiths.com>
2020-01-05 19:38:46 +0100

    WIP.Refactor
    - Added Quote component
    - Added Ajax Error Handle
    - Updated CSS for error handle

--- examples/simple-api/src/App.js
+++ examples/simple-api/src/App.js
@@ -8,43 +8,60 @@ WebFont.load({
     families: ['Acme:400', 'sans-serif']
   }
 });
-function App() {
+
+function Quote() {
   const [quote, setQuote] = useState()
+  const [err, setErr] = useState()
 
   useEffect(() => {
     fetch("https://api.chucknorris.io/jokes/random")
       .then(res => res.json())
       .then(data => {
-        console.log("Data is here", data)
         setQuote(data.value)
       })
-      .catch(err => {
-        console.warn(err)
+      .catch(error => {
+        console.warn(error)
+        setErr(error)
       })
   }, [])
+
+  if(err) {
+    return (
+      <React.Fragment>
+        <h1 className="App-error">Critical Error❗️</h1>
+        <h3>Please Reload</h3>
+      </React.Fragment>
+    )
+  }
+
+  if(!quote){
+    return <h1>Loading....</h1>
+  }
+
+  return (
+    <React.Fragment>
+      <img src={chuckPic} className="App-pic" alt="logo" />
+      <p>
+      {quote}
+      </p>
+      <a
+        className="App-link"
+        href="https://chucknorris.io"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Quote by chucknorris.io
+      </a>
+    </React.Fragment>
+  )
+}
+
+function App() {
 
   return (
     <div className="App">
       <header className="App-header">
-        {!quote ? (
-        <h1>Loading....</h1>
-        ) : (
-        <React.Fragment>
-          <img src={chuckPic} className="App-pic" alt="logo" />
-          <p>
-          {quote}
-          </p>
-          <a
-            className="App-link"
-            href="https://chucknorris.io"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Quote by chucknorris.io
-          </a>
-        </React.Fragment>
-        )}
-
+        <Quote/>
       </header>
     </div>
   );

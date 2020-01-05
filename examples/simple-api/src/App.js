6eac519cd1cc3ca1716386c4288f366e1fb13d19
Ulises Gascon <ulises.gascon@guidesmiths.com>
2020-01-05 18:45:13 +0100

    Added Chuck Norris to project

--- examples/simple-api/src/App.js
+++ examples/simple-api/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import chuckPic from './chuck_norris.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={chuckPic} className="App-pic" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
